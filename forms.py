2f433235f54cd55d52bf183f34c766db2406e038
[odicheobiakarije@gmail.com] <[odicheobiakarije@gmail.com]>
2019-06-21 15:42:27 -0400

    form saving

--- forms.py
+++ forms.py
@@ -1,4 +1,8 @@
 from flask_wtf import FlaskForm
-from wtform import StringField
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
 
-class RegistrationForm(FlaskForm)
+class RegistrationForm(FlaskForm):
+        username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+        email = StringField('Email', validators=[DataRequired(), Email()])
