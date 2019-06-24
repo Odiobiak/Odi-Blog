a34c9174535f93e61bc6aa369b5b88314dc2953b
[odicheobiakarije@gmail.com] <[odicheobiakarije@gmail.com]>
2019-06-24 12:23:47 -0400

    more changes! forms, login regtister nd more

--- flaskblog.py
+++ flaskblog.py
@@ -1,33 +1,57 @@
+from flask import Flask, render_template, url_for, flash, redirect
 
-from flask import Flask, render_template, url_for
+from forms import LoginForm, RegistrationForm
+
 app = Flask(__name__)
 
+#To set a secret key to avoid invalid access
+app.config['SECRET_KEY'] = 'fc5737e4e9c32412f223f91277f72b62'
+
 #creating a post!
-posts = [
-    {
-        'author' : 'Odiche Obiakarije',
-        'title': 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'April 20, 2019'
-    },
-    {
-        'author' : 'Grace Daniels',
-        'title': 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'April 21, 2019'
-    }
-]
+posts = [{
+    'author': 'Odiche Obiakarije',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'April 20, 2019'
+}, {
+    'author': 'Grace Daniels',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 21, 2019'
+}]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
 
-#for continuous runing stuff. help so you dont have to quit and reconnect over nd over again!
+#creating a route for register and login feature
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()  #creating an instance of the form
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()  #creating an instance of the form.py
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/Author-Note")
+def author_note():
+    return render_template('author_note.html')
+
+
+#for continuous run. help so you dont have to quit and reconnect over nd over again!
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,8 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, equal_to
 
+#To register a user
 class RegistrationForm(FlaskForm):
         username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
 
         email = StringField('Email', validators=[DataRequired(), Email()])
+
+        password = PasswordField('Password', validators=[DataRequired()])
+        confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), equal_to('password')])
+
+        submit = SubmitField('Sign Up')
+
+#The user login
+
+class LoginForm(FlaskForm):
+       # username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+        email = StringField('Email', validators=[DataRequired(), Email()])
+
+        password = PasswordField('Password', validators=[DataRequired()])
+        #confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), equal_to('password')])
+
+        remember = BooleanField('Remeber Me')
+        submit = SubmitField('Sign Up')
