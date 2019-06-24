851af17c7e8f389120aa90241e77b52ef84289bd
[odicheobiakarije@gmail.com] <[odicheobiakarije@gmail.com]>
2019-06-24 15:45:11 -0400

    fd

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-
+from flask_sqlalchemy import SQLAlchemy 
 from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
