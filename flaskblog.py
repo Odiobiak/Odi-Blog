2f710a64bf680540f51d79e712f9f638e9378d73
[odicheobiakarije@gmail.com] <[odicheobiakarije@gmail.com]>
2019-06-25 13:54:37 -0400

    db

--- flaskblog.py
+++ flaskblog.py
@@ -1,3 +1,4 @@
+from datetime import datetime 
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from forms import LoginForm, RegistrationForm
@@ -6,6 +7,26 @@ app = Flask(__name__)
 
 #To set a secret key to avoid invalid access
 app.config['SECRET_KEY'] = 'fc5737e4e9c32412f223f91277f72b62'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  #creating a db to store the information
+#creating an instance of the database
+db = SQLAlchemy(app)
+
+#creating user models with the db
+class User(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), nullable =False)
+    date_posted = db.Column(db.DateTime, nullable=False, default= datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
 
 #creating a post!
 posts = [{
