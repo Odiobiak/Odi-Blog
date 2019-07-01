5269c1d8dd7daa987b642ff9b85dceb31555d983
[odicheobiakarije@gmail.com] <[odicheobiakarije@gmail.com]>
2019-07-01 13:47:20 -0400

    0

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = 'fc5737e4e9c32412f223f91277f72b62'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  #creating a db to store the information on the same directory 
 #creating an instance of the database
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from flaskblog import routes
+from flaskblog import routes  
