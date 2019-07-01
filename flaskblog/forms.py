942d0dc4c2ce705bc596c1f83bfa27e37d46c3b7
[odicheobiakarije@gmail.com] <[odicheobiakarije@gmail.com]>
2019-07-01 15:43:13 -0400

    f

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, equal_to
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 #To register a user
 class RegistrationForm(FlaskForm):
@@ -9,12 +10,21 @@ class RegistrationForm(FlaskForm):
         email = StringField('Email', validators=[DataRequired(), Email()])
 
         password = PasswordField('Password', validators=[DataRequired()])
-        confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), equal_to('password')])
+        confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
         submit = SubmitField('Sign Up')
 
-#The user login
+        def validate_username(self, username):
+                user = User.query.filter_by(username=username.data).first()
+                if user:
+                        raise ValidationError('That username is taken. Please choose a different username!')
 
+        def validate_email(self, email):
+                user = User.query.filter_by(email=email.data).first()
+                if user:
+                        raise ValidationError('That email is taken. Please choose a different email!')
+
+#The user login
 class LoginForm(FlaskForm):
        # username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
 
